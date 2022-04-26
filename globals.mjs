bfafac3d330e67820f2d9ea8148d105138d229f0
Carlos Alberto Castaño <calbertts@gmail.com>
2022-04-26 16:49:20 +0200

    Adding memory usage output

--- globals.mjs
+++ globals.mjs
@@ -1,8 +1,11 @@
 import http from "http";
+import { memoryUsage } from 'process';
 
 const requestLogs = [];
 const server = http.createServer((req, res) => {
   requestLogs.push({ url: req.url, array: new Array(100000).join("*") });
+
+  console.log(parseInt(memoryUsage.rss() / (1024*1024)));
 
   res.end(JSON.stringify(requestLogs));
 });
