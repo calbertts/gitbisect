299d79e5d4333f2332cb89fcb0c48d1c741337be
Carlos Alberto Castaño <calbertts@gmail.com>
2022-04-26 11:41:41 +0200

    Adding logs array

--- globals.mjs
+++ globals.mjs
@@ -1,6 +1,8 @@
 import http from "http";
 
+const requestLogs = [];
 const server = http.createServer((req, res) => {
+  requestLogs.push({ url: req.url, array: new Array(100000).join("*") });
   res.end();
 });
 
